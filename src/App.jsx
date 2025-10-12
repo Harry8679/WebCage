a6722a227008b197fb216ab55ce7f71d6ed4bf74
Harry7986 <emarh.harry.code@gmail.com>
2025-10-12 12:52:20 +0200

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
+// src/App.jsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import LandingPage from "./pages/LandingPage";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
 
 export default function App() {
   return (
     <BrowserRouter>
+      {/* Navbar globale */}
+      <Navbar variant="glass" />
+
+      {/* Routes */}
       <Routes>
-        <Route element={<LandingPage />} path='/' />
+        <Route path="/" element={<LandingPage />} />
+        {/* <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} /> */}
       </Routes>
     </BrowserRouter>
   );
