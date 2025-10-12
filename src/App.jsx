1c525ed23802d5b49b5f27feb93127f50af51fec
Harry7986 <emarh.harry.code@gmail.com>
2025-10-12 06:28:28 +0200

    App 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<}
+        <Route element={<LandingPage />} path='/' />
       </Routes>
     </BrowserRouter>
   );
