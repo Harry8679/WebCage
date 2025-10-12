2e01ea5ab887b6301b9e542d2cd061fdb34e45fc
Harry7986 <emarh.harry.code@gmail.com>
2025-10-12 12:56:46 +0200

    App 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 // src/App.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import Navbar from "./components/Navbar";
 import LandingPage from "./pages/LandingPage";
 // import Login from "./pages/Login";
 // import Register from "./pages/Register";
+import { logout } from "./features/auth/authSlice";
 
 export default function App() {
+  const dispatch = useDispatch();
+  const user = useSelector((s) => s.auth.user);
+
   return (
     <BrowserRouter>
-      {/* Navbar globale */}
-      <Navbar variant="glass" />
+      <Navbar
+        user={user}                       // { firstName?, email }
+        onLogout={() => dispatch(logout())}
+        variant="glass"
+      />
 
-      {/* Routes */}
       <Routes>
         <Route path="/" element={<LandingPage />} />
         {/* <Route path="/login" element={<Login />} />
