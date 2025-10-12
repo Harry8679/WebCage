c5ad4e3c44ff8d3b6f6e1a888d8c7cee3f9541c5
Harry7986 <emarh.harry.code@gmail.com>
2025-10-12 06:23:46 +0200

    App 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 export default function App() {
   return (
-    <div className="min-h-screen grid place-items-center justify-center bg-gray-50">
-      <div className="p-6 rounded-2xl shadow bg-white">
-        <h1 className="text-2xl font-bold">Transkage + Tailwind v3</h1>
-        <p className="text-gray-600 mt-2">Ça fonctionne 🎉</p>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<}
+      </Routes>
+    </BrowserRouter>
   );
 }
